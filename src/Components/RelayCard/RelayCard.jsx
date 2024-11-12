8467bd87215c09d1f510253a53897dab2b80a0c8
Umer Bandey <umerbandey@Umers-MacBook-Air.local>
2024-11-12 12:07:22 +0530

    changes

--- src/Components/RelayCard/RelayCard.jsx
+++ src/Components/RelayCard/RelayCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LazyImage from '../../utils/LazyImaga'
-import { Card,Typography,AspectRatio,CardContent,Button } from '@mui/joy';
+import { Card,Typography,AspectRatio,CardContent,Link, Chip,Divider } from '@mui/joy';
 
 const RelayCard = ({ relay }) => {
     return (
@@ -13,20 +13,42 @@ const RelayCard = ({ relay }) => {
       <AspectRatio color='white' minHeight="120px" maxHeight="200px">
         <LazyImage src={relay.image}/>
       </AspectRatio>
-      <CardContent orientation="horizontal">
-        <div>
-          <Typography level="body-xs">Total price:</Typography>
-          <Typography sx={{ fontSize: 'lg', fontWeight: 'lg' }}>{relay.price}</Typography>
+      <CardContent sx={{borderColor:'lightgrey',borderStyle:"solid",borderWidth:"0.2px",borderRadius:"5px",padding:"20px",paddingTop:"0px",paddingBottom:"10px",boxSizing:"border-box"}} orientation="vertical">
+     
+      <Divider sx={{ '--Divider-childPosition': `50%` ,fontWeight:"400",marginTop:"10px",marginBottom:"10px"}}>
+    Functions Available
+  </Divider>
+        <div >
+          
+          {relay.specifications.protectionFunctions.map((protectionFunction, index) => (
+            <Chip size='sm' variant='soft' key={index} sx={{ mr: 1, mb: 1 }} >
+              {protectionFunction}
+            </Chip>
+          ))}
         </div>
-        <Button
-          variant="solid"
-          size="md"
-          color="primary"
-          aria-label="Explore Bahamas Islands"
-          sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-        >
-          Explore
-        </Button>
+        <Divider sx={{ '--Divider-childPosition': `50%` ,fontWeight:"400",marginTop:"10px",marginBottom:"10px"}}>
+    Setting Calculators
+  </Divider>
+  <div >
+          
+          {relay.specifications.protectionFunctions.map((protectionFunction, index) => (
+            <Chip onClick={()=>{}} size='sm' color='primary' variant='outlined' key={index} sx={{ mr: 1, mb: 1 }} >
+              {protectionFunction}
+            </Chip>
+          ))}
+        </div>
+        <Divider sx={{ '--Divider-childPosition': `50%`,fontWeight:"400" ,marginTop:"10px",marginBottom:"10px"}}>
+    Manuals and References
+  </Divider>
+  <div >
+          
+          {relay.specifications.protectionFunctions.map((protectionFunction, index) => (
+            <Link level="body-sm" key={index} sx={{ mr: 2, mb: 1 }} >
+              {protectionFunction}
+            </Link>
+          ))}
+        </div>
+        
       </CardContent>
     </Card>
     );
